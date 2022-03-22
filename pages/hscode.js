5325f5b3a9b13f1680e027444dfbd8bbe96f2ba5
Shekhar <shekhar@MAC16.local>
2022-03-22 22:48:17 +0530

    updated hscode

--- pages/hscode.js
+++ pages/hscode.js
@@ -1,3 +1,14 @@
-export default function Port(){
-    return (<div><h1>this is HS Code</h1></div>);
+import { useState ,useEffect} from "react";
+export default function Hscode(){
+    const[data,setData]=useState(null);
+  useEffect(()=>{
+    console.log("enter....");
+   fetch("https://staging-api.wizfreight.com/v1/hs-codes").then((res)=>res.json()).then((dt)=>{setData(dt)});
+   console.log("exit",data);
+  },[])
+  return (
+    <div className="App">
+      {data&& data.hs_codes.map((ele)=><div>{ele.name},{ele.code},</div>)}
+      </div>
+  );
 }
