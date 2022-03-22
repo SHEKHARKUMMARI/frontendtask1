c0f6ea341f035f67a8168d3fe075a17d6d29a735
Dinesh Kumar <dineshkumar@MAC15.local>
2022-03-23 00:02:42 +0530

    fixed

--- pages/port.js
+++ pages/port.js
@@ -6,21 +6,28 @@ export default function Port(){
    fetch("https://staging-api.wizfreight.com/v1/ports").then((res)=>res.json()).then((dt)=>{setData(dt)});
   },[])
 
+  
+
   return (
     <div className="App">
       {data&& data.ports.map((curElem)=> {
+        const {name} = curElem;
+        const {code} = curElem.info.city.info.country;
+        const city = curElem.info.city.name;
+        const {state} = curElem.info;
+        const country = curElem.info.city.info.country.name
+        
         return (
             <div key={curElem.id}>
-                <h3>Name : <span>{curElem.name}</span></h3>
-                <h3>Code : <span>{curElem.info.city.info.country.code}</span></h3>
-                <h3>City : <span>{curElem.info.city.name}</span></h3>
-                <h3>State : <span>{curElem.info.state}</span></h3>
-                <h3>Country : <span>{curElem.info.city.info.country.name}</span></h3>
+                <h3>Name : <span>{name}</span></h3>
+                <h3>Code : <span>{code}</span></h3>
+                <h3>City : <span>{city}</span></h3>
+                <h3>State : <span>{state}</span></h3>
+                <h3>Country : <span>{country}</span></h3>
             </div>
           )
-      })
-      
-      }
+      })      
+      } 
       </div>
   );
 }
