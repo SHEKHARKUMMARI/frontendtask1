c97a31fcd5f6026fba94d34112cd4a6c27c6486d
Shekhar <shekhar@MAC16.local>
2022-03-23 10:49:47 +0530

    added pagination

--- pages/port.js
+++ pages/port.js
@@ -1,14 +1,30 @@
 import { useState ,useEffect} from "react";
+import StickyHeadTable from "../materialui/pagenation"
 export default function Port(){
     const [data, setData] = useState(null);
-  
+      const [row,setRow]=useState([]);
   useEffect(()=>{
    fetch("https://staging-api.wizfreight.com/v1/ports").then((res)=>res.json()).then((dt)=>{setData(dt)});
   },[])
-
+  useEffect(()=>{
+   if(data){
+       const rw=data.ports.map((curElem)=> { 
+          const Name=curElem.name;
+          const Code=curElem.info.city.info.country.code ;
+          const City=curElem.info.city.name;
+          const State=curElem.info.state;
+          const Country=curElem.info.city.info.country.name;
+           return {Name,Code,City,State,Country}
+       })
+       setRow(rw);
+   }      
+   
+  },[data])
+  useEffect(()=>{console.log(row)});
   return (
     <div className="App">
-      {data&& data.ports.map((curElem)=> {
+    {data&&<StickyHeadTable rows={row} />}
+      {/* {data&& data.ports.map((curElem)=> {
         return (
             <div key={curElem.id}>
                 <h3>Name : <span>{curElem.name}</span></h3>
@@ -18,9 +34,10 @@ export default function Port(){
                 <h3>Country : <span>{curElem.info.city.info.country.name}</span></h3>
             </div>
           )
-      })
-      
-      }
+      }) 
+      } */}
       </div>
   );
 }
+
+  
