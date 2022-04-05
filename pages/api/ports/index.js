02c265eb78d45be7c72fe3f6ce87e3b7eeea2033
Dinesh Kumar <dineshkumar@MAC15.local>
2022-04-05 23:48:04 +0530

    added endpoint POST

--- pages/api/ports/index.js
+++ pages/api/ports/index.js
@@ -2,13 +2,14 @@
 import sqlite from 'sqlite';
 export default async function getAllPersonById(req, res) {
   const db=await sqlite.open('./mydb.sqlite');
-  if(req.method === 'PUT')
+  if(req.method === 'POST')
   {
-    const statement=await db.prepare('UPDATE Person SET name=?,email=? WHERE id=?');
-    const result=statement.run(req.body.name,req.body.email,req.query.id);
+    const statement=await db.prepare('INSERT INTO Port(name,code,city,state,country) VALUES(?,?,?,?,?)'); //name,code,city,state,country
+    const result=statement.run(req.body.name,req.body.code,req.body.city,req.body.state,req.body.country);
     (await result).finalize();
   }
 const port=await db.all('SELECT*FROM Port');
+
 
   res.json(port);
 }
